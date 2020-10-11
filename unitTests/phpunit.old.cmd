4e1da542fb24b1eefca0141d90a99e73c92567f9
Owen Leibman <eclipsechasers2@yahoo.com>
2020-10-10 19:10:19 -0700

    Suppress libxml_disable_entity_loader for PHP8+
    No tests involved.

--- unitTests/phpunit.old.cmd
+++ unitTests/phpunit.old.cmd
@@ -1,3 +1,2 @@
-@\wamp64\bin\php\php7.3.13\php.exe "%~dp0phpunit-6.5.14.phar" %* 
-@rem below doesn't work: Function ReflectionType::__toString() is deprecated 
-@rem \wamp64\bin\php\php7.4.1\php.exe "%~dp0phpunit-6.5.14.phar" %* 
+@rem run with < PHP7.4
+@call php73 "%~dp0phpunit-6.5.14.phar" %* 
